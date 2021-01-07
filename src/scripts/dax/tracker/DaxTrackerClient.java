4cc3f1f159684540c4c8dadae9323f3bceda82f0
dax <dax>
2021-01-06 18:12:17 -0800

    update local

--- src/scripts/dax/tracker/DaxTrackerClient.java
+++ src/scripts/dax/tracker/DaxTrackerClient.java
@@ -14,7 +14,7 @@ import static org.tribot.api.General.getTRiBotUserID;
 
 class DaxTrackerClient {
 
-    private static final String BASE_URL = "http://localhost:8080/api/";
+    private static final String BASE_URL = "https://tracker.dax.cloud/api/";
 
     private final OkHttpClient okHttpClient;
     private final Gson gson;
